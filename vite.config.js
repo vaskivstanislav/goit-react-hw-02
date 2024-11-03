aec83b2bd0893213bb9034b0dd556d528f829007
vaskivstanislav <stasmegatyr@gmail.com>
2024-11-03 17:52:46 +0200

    fix fn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/goit-react-hw-02/",
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  }
 });
