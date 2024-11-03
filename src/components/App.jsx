d8070ddbe6def0f2d27598b9c8a6700ae3efbaab
vaskivstanislav <stasmegatyr@gmail.com>
2024-11-03 17:10:04 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,59 @@
-import React from "react";
-import Profile from "./Profile/Profile";
-import userData from "../userData.json";
-import friends from "../friends.json";
-import transactions from "../transactions.json";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import Description from './components/Description/Description';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
+import { useEffect, useState } from 'react';
 
+function App() {
+  const [comment, setComment] = useState(() => {
+    if (window.localStorage.getItem('commentStatus')) {
+      const feedbackLocal = window.localStorage.getItem('commentStatus');
+      return JSON.parse(feedbackLocal);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
 
-const App = () => {
-  return (
-    <div>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
-    
+  const totalFeedback = comment.good + comment.neutral + comment.bad;
+  const positiveFeedback = Math.round(
+    ((comment.good + comment.neutral) / totalFeedback) * 100
   );
-};
+
+  useEffect(() => {
+    window.localStorage.setItem('commentStatus', JSON.stringify(comment));
+  }, [comment]);
+
+  const updateFeedback = feedbackType =>
+    setComment(prev => ({ ...prev, [feedbackType]: prev[feedbackType] + 1 }));
+
+  const ressetFeedback = () =>
+    setComment({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        ressetFeedback={ressetFeedback}
+      />
+      {totalFeedback > 0 && (
+        <Feedback
+          comment={comment}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )}
+      {totalFeedback === 0 && <Notification />}
+    </>
+  );
+}
 
 export default App;
